00ebae876359bfc56c1019be2c288219317cbf62
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-11-19 17:53:57 +0200

    ass and ev by id

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EventService } from './event.service';
 import { Event } from './event.entity';
 
@@ -7,7 +7,12 @@ export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Get()
-  getHello(): Promise<Event[]> {
+  getEvents(): Promise<Event[]> {
     return this.eventService.findAll();
+  }
+
+  @Get('/:id')
+  getEventById(@Param('id') id: number): Promise<Event> {
+    return this.eventService.findOne(id);
   }
 }
